b7363aa7ff96a0c72b312a569c0b75664d44df12
huzongyao <huzongyao@tangbull.com>
2024-06-22 07:22:37 +0800

    lv8 demo

---
+++ WT32-PLUS-LV8-Demo/README.md
@@ -0,0 +1,2 @@
+# WT32-SC01-LVDemo
+LVGL 8 Demo for WT32-SC01 With Arduino
---
+++ WT32-PLUS-LV8-Demo/misc/merge.py
@@ -0,0 +1,28 @@
+Import('env')
+import os
+
+OUTPUT_DIR = "$BUILD_DIR{}".format(os.path.sep)
+APP_BIN = "$BUILD_DIR/${PROGNAME}.bin"
+
+
+def copy_merge_bins(source, target, env):
+    firmware_src = str(target[0])
+    flash_images = env.Flatten(env.get("FLASH_EXTRA_IMAGES", [])) + ["$ESP32_APP_OFFSET", APP_BIN]
+    name = firmware_src.split(os.path.sep)[2]
+    flash_size = env.GetProjectOption("board_upload.flash_size")
+    board = env.BoardConfig()
+    f_flash = board.get("build.f_flash", "40000000L")
+    flash_freq = '40m'
+    if (f_flash == '80000000L'):
+        flash_freq = '80m'
+    mcu = board.get("build.mcu", "esp32")
+    firmware_dst = "{}lv8demo_{}_{}_{}_0x0.bin".format(OUTPUT_DIR, mcu, name, flash_size)
+    if os.path.isfile(firmware_dst):
+        os.remove(firmware_dst)
+    cmd = " ".join(
+        ["$PYTHONEXE", "$OBJCOPY", '--chip', mcu, 'merge_bin', '--output', firmware_dst, '--flash_mode', 'dio',
+         '--flash_size', flash_size, '--flash_freq', flash_freq] + flash_images)
+    env.Execute(cmd)
+
+
+env.AddPostAction("$BUILD_DIR/${PROGNAME}.bin", [copy_merge_bins])
